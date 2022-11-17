5a40857727365f2b4a9f98d2b3231c3f0f45d323
jacob <jacobkon1401@gmail.com>
2022-11-16 20:52:16 -0500

    added more features

--- app.js
+++ app.js
@@ -1,27 +1,67 @@
 let zip =document.querySelector(".zip")
 let button =document.querySelector(".findZman")
+let errorMessage = document.querySelector(".error")
+let loc =document.querySelector("#location")
+let sunrise =document.querySelector("#sunrise")
+let sunset =document.querySelector("#sunset")
+let validZip =false
 
+ //function to clear all feilds
+ function clearFields(){
+  loc.innerText=""
+  sunrise.innerText=""
+  sunset.innerText=""
+ }
 
-button.addEventListener('click',()=>{
-  //add validator for a valid zip
-  zmanChecker(zip.value)
-})
-let zmanChecker = async(zipp)=>{
- 
+//function to get zmanim with axios using user proided zip
+let zmanChecker = async(zipCode)=>{
   let todaysDate =new Date();
   let d = todaysDate.toISOString().slice(0,10);
   try{
-      let response = await axios.get(`https://www.hebcal.com/zmanim?cfg=json&zip=${zipp}&date=${d}`)
+      let response = await axios.get(`https://www.hebcal.com/zmanim?cfg=json&zip=${zipCode}&date=${d}`)
       console.log(response.data)
-      // let day =  response.data.date;
+      loc.innerText=response.data.location.title
+      sunrise.innerText=response.data.times.sunrise
+      sunset.innerText=response.data.times.sunset
       console.log()
       console.log('**************************')
       console.log(response.data.times)
+
   }
   catch(error){
       console.error('error cought by me',error);
+      errorMessage.innerText ="please enter a valid zipcode"
+      setTimeout(()=>{
+        errorMessage.innerText =""
+      },5000)
+      
   }
  };
+
+
+ function zipValidator(zip){ //checks for exactly five digits in user zip
+    if(/^\d{5}$/.test(zip)){
+      console.log('thanks that is a valid zip')
+      return validZip = true
+
+    }else{
+      console.log('pleses enter valid zip')
+      errorMessage.innerText ="please enter a valid zipcode"
+      setTimeout(()=>{
+        errorMessage.innerText =""
+      },2000)
+      return validZip = false
+    }
+ }
+ 
+
+ button.addEventListener('click',()=>{
+  clearFields()
+  zipValidator(zip.value)
+  if (validZip){
+    zmanChecker(zip.value)
+  }
+})
 
 
 
